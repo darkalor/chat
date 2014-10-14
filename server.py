b5f1c00e53d43ff80744902737f495da892d5034
darkalor <darkalor@gmail.com>
2014-10-14 21:50:59 +0300

    Started server implementation

--- server.py
+++ server.py
@@ -1,66 +1,72 @@
-# Tcp Chat server
- 
-import socket, select
- 
-#Function to broadcast chat messages to all connected clients
-def broadcast_data (sock, message):
-    #Do not send the message to master socket and the client who has send us the message
-    for socket in CONNECTION_LIST:
-        if socket != server_socket and socket != sock :
-            try :
-                socket.send(message)
-            except :
-                # broken socket connection may be, chat client pressed ctrl+c for example
-                socket.close()
-                CONNECTION_LIST.remove(socket)
- 
-if __name__ == "__main__":
-     
-    # List to keep track of socket descriptors
-    CONNECTION_LIST = []
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = 5000
-     
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
-    server_socket.listen(10)
- 
-    # Add server socket to the list of readable connections
-    CONNECTION_LIST.append(server_socket)
- 
-    print "Chat server started on port " + str(PORT)
- 
-    while 1:
-        # Get the list sockets which are ready to be read through select
-        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
- 
-        for sock in read_sockets:
-            #New connection
-            if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
-                sockfd, addr = server_socket.accept()
-                CONNECTION_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
-                 
-                broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
-             
-            #Some incoming message from a client
-            else:
-                # Data recieved from client, process it
+#!/usr/bin/python
+import socket
+import select
+
+
+class Server():
+
+    _connectionList = []
+
+    def __init__(self, port, buffer=4096):
+        self._port = port
+        self._buffer = buffer
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._socket.bind(("0.0.0.0", self._port))
+        self._socket.listen(10)
+        self._connectionList.append(self._socket)
+
+    def __del__(self):
+        self._socket.close()
+
+    def broadcast(self, sock, message):
+        #Do not send the message to master socket and the client who has send us the message
+        for connection in self._connectionList:
+            if connection != self._socket and connection != sock:
                 try:
-                    #In Windows, sometimes when a TCP program closes abruptly,
-                    # a "Connection reset by peer" exception will be thrown
-                    data = sock.recv(RECV_BUFFER)
-                    if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)               
-                 
-                except:
-                    broadcast_data(sock, "Client (%s, %s) is offline" % addr)
-                    print "Client (%s, %s) is offline" % addr
-                    sock.close()
-                    CONNECTION_LIST.remove(sock)
-                    continue
-     
-    server_socket.close()
+                    connection.send(message)
+                except Exception as e:
+                    # broken socket connection may be, chat client pressed ctrl+c for example
+                    connection.close()
+                    self._connectionList.remove(connection)
+
+    def run(self):
+        while 1:
+            # Get the list sockets which are ready to be read through select
+            try:
+                readSockets, writeSockets, errorSockets = select.select(self._connectionList, [], [])
+
+                for sock in readSockets:
+                    #New connection
+                    if sock == self._socket:
+                        # Handle the case in which there is a new connection received through server_socket
+                        connection, address = self._socket.accept()
+                        self._connectionList.append(connection)
+                        print "Client (%s, %s) connected" % address
+                        self.broadcast(connection, "[%s:%s] entered room\n" % address)
+                    #Some incoming message from a client
+                    else:
+                        # Data received from client, process it
+                        data = sock.recv(self._buffer)
+                        if data:
+                            # Address of socket (client)
+                            self.broadcast(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)
+                        else:
+                            self.broadcast(sock, "Client (%s, %s) is offline\n" % address)
+                            print "Client (%s, %s) is offline" % address
+                            sock.close()
+                            self._connectionList.remove(sock)
+            except KeyboardInterrupt:
+                #TODO: socket not closed on ctrl+c
+                print "Exiting"
+                self._socket.shutdown(socket.SHUT_RDWR)
+                self._socket.close()
+                exit()
+
+
+if __name__ == "__main__":
+    port = 5000
+    server = Server(port)
+    print "Chat server started on port " + str(port)
+    server.run()
+
